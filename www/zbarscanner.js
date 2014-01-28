7582f6e7e43829fc7f9c7b1333f713a836ce6b1d
Michael Black <michael.black@collectme.co>
2014-01-28 15:41:31 +1100

    minor JS fixes

--- www/zbarscanner.js
+++ www/zbarscanner.js
@@ -21,14 +21,14 @@
              *
              * @type Object
              */
-            this.Encode = {
-                TEXT_TYPE: "TEXT_TYPE",
-                EMAIL_TYPE: "EMAIL_TYPE",
-                PHONE_TYPE: "PHONE_TYPE",
-                SMS_TYPE: "SMS_TYPE"
-                //  CONTACT_TYPE: "CONTACT_TYPE",  // TODO:  not implemented, requires passing a Bundle class from Javascript to Java
-                //  LOCATION_TYPE: "LOCATION_TYPE" // TODO:  not implemented, requires passing a Bundle class from Javascript to Java
-            };
+            // this.Encode = {
+            //     TEXT_TYPE: "TEXT_TYPE",
+            //     EMAIL_TYPE: "EMAIL_TYPE",
+            //     PHONE_TYPE: "PHONE_TYPE",
+            //     SMS_TYPE: "SMS_TYPE"
+            //     //  CONTACT_TYPE: "CONTACT_TYPE",  // TODO:  not implemented, requires passing a Bundle class from Javascript to Java
+            //     //  LOCATION_TYPE: "LOCATION_TYPE" // TODO:  not implemented, requires passing a Bundle class from Javascript to Java
+            // };
         };
 
         /**
@@ -61,27 +61,27 @@
         };
 
         //-------------------------------------------------------------------
-        ZBarScanner.prototype.encode = function (type, data, successCallback, errorCallback, options) {
-            if (errorCallback == null) {
-                errorCallback = function () {
-                };
-            }
+        // ZBarScanner.prototype.encode = function (type, data, successCallback, errorCallback, options) {
+        //     if (errorCallback == null) {
+        //         errorCallback = function () {
+        //         };
+        //     }
 
-            if (typeof errorCallback != "function") {
-                console.log("BarcodeScanner.encode failure: failure parameter not a function");
-                return;
-            }
+        //     if (typeof errorCallback != "function") {
+        //         console.log("BarcodeScanner.encode failure: failure parameter not a function");
+        //         return;
+        //     }
 
-            if (typeof successCallback != "function") {
-                console.log("BarcodeScanner.encode failure: success callback parameter must be a function");
-                return;
-            }
+        //     if (typeof successCallback != "function") {
+        //         console.log("BarcodeScanner.encode failure: success callback parameter must be a function");
+        //         return;
+        //     }
 
-            exec(successCallback, errorCallback, 'BarcodeScanner', 'encode', [
-                {"type": type, "data": data, "options": options}
-            ]);
-        };
+        //     exec(successCallback, errorCallback, 'BarcodeScanner', 'encode', [
+        //         {"type": type, "data": data, "options": options}
+        //     ]);
+        // };
 
-        var barcodeScanner = new BarcodeScanner();
-        module.exports = barcodeScanner;
+        var zbarScanner = new ZBarScanner();
+        module.exports = zbarScanner;
 
